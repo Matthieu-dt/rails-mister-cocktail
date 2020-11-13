e8c26334a7773c56deb0f10a435789de2b31819c
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-13 11:28:29 +0100

    new route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized_data = open(url).read
+data = JSON.parse(serialized_data)
+data['drinks'].each do |ingredient|
+  puts "Printing #{ingredient['strIngredient1']}"
+  new_ingredient = Ingredient.new(
+    name: ingredient['strIngredient1']
+  )
+  new_ingredient.save!
+end
